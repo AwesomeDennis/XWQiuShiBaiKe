e34fa4a674e23ed3ab10838c25e78f713fb60a86
renxinwei <362922604@qq.com>
2013-05-24 14:31:12 +0800

    init project.

---
+++ XWQiuShiBaiKe/XWQiuShiBaiKe/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  XWQiuShiBaiKe
+//
+//  Created by Ren XinWei on 13-5-24.
+//  Copyright (c) 2013年 renxinwei's iMac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
