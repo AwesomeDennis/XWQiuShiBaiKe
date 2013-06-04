f79acddd892066847c48294a966c593b83ba1fd6
renxinwei <362922604@qq.com>
2013-06-04 16:18:41 +0800

    添加带占位符的UITextView

---
+++ XWQiuShiBaiKe/XWQiuShiBaiKe/Utils/Custom/XWCustomView/XWPlaceHolderTextView.h
@@ -0,0 +1,20 @@
+//
+//  XWPlaceHolderTextView.h
+//  XWQiuShiBaiKe
+//
+//  Created by Ren XinWei on 13-6-4.
+//  Copyright (c) 2013年 renxinwei's MacBook Pro. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface XWPlaceHolderTextView : UITextView
+
+@property (nonatomic, retain) NSString *placeHolder;
+@property (nonatomic, retain) UIColor *placeHolderColor;
+@property (nonatomic, retain) UILabel *placeHolderLabel;
+@property (nonatomic, retain) UIImage *placeHodlerBackgroundImage;
+
+- (void)textChanged:(NSNotification *)notification;
+
+@end
