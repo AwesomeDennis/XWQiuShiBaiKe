0f94120ecb9a1d49d8e0d83b094465abe19db2fc
renxinwei <renxinwei@renxinweitekiMacBook-Pro.local>
2013-06-04 18:55:02 +0800

    add uicolor category

---
+++ XWQiuShiBaiKe/XWQiuShiBaiKe/Utils/Custom/XWCategory/UIColor+XWCategory.h
@@ -0,0 +1,17 @@
+//
+//  UIColor+XWCategory.h
+//  XWQiuShiBaiKe
+//
+//  Created by renxinwei on 13-6-4.
+//  Copyright (c) 2013年 renxinwei's MacBook Pro. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIColor (XWCategory)
+
++ (UIColor*)colorWithHex:(long)hexColor;
++ (UIColor *)colorWithHex:(long)hexColor alpha:(float)opacity;
++ (UIColor *)colorWithHexString:(NSString *)hexColor;
+
+@end
