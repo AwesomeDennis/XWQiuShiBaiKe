5e192cbc3eda038b0afa13bf4e47e00c9bb02270
renxinwei <362922604@qq.com>
2013-07-09 11:14:43 +0800

    Add UIImage category

---
+++ XWQiuShiBaiKe/XWQiuShiBaiKe/Utils/Custom/XWCategory/UIImage+XWCategory.m
@@ -0,0 +1,13 @@
+//
+//  UIImage+XWCategory.m
+//  XWQiuShiBaiKe
+//
+//  Created by Ren XinWei on 13-7-9.
+//  Copyright (c) 2013年 renxinwei's MacBook Pro. All rights reserved.
+//
+
+#import "UIImage+XWCategory.h"
+
+@implementation UIImage (XWCategory)
+
+@end
